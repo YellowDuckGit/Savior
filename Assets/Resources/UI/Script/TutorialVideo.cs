ed4204213d9dbf2135bc8df2f1ac6c9e94666bd1
thanhtuyenne <88424555+thanhtuyenne@users.noreply.github.com>
2023-07-22 20:05:36 +0700

    upload sound

---
+++ Assets/Resources/UI/Script/TutorialVideo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.Video;
+
+namespace Assets.Resources.UI.Script
+{
+    public class TutorialVideo : MonoBehaviour
+    {
+        private VideoPlayer player;
+
+        private void Start()
+        {
+            player = GetComponent<VideoPlayer>();
+        }
+
+        public void ContinueVid()
+        {
+            player.Play();
+        }
+
+        public void StopVid()
+        {
+            player.Stop();
+        }
+    }
+}
